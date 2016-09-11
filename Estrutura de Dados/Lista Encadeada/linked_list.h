25469d065c173a83dddf0e036b73564860f82b10
danielboso <bosodaniel@hotmail.com>
2016-09-11 11:12:53 -0300

    lista encadeada com documentação doxygen

--- Estrutura de Dados/Lista Encadeada/linked_list.h
+++ Estrutura de Dados/Lista Encadeada/linked_list.h
@@ -16,39 +16,36 @@ namespace structures {
 template<typename T>
 //! Lista encadeada
 class LinkedList {
-public:
-
+ public:
     //! Construtor
     /*! 
         Construtor padrão da classe. Inicializa a lista com um tamanho padrão.
     */
-    LinkedList() { // construtor padrão
+    LinkedList() {  // construtor padrão
         head = nullptr;
         size_ = 0;
     }
-    
+
     //! Destrutor
     /*!
         Destrutor da classe.
     */
-    ~LinkedList() { // destrutor
+    ~LinkedList() {  // destrutor
         clear();
-    } 
+    }
 
     //! Função clear
     /*!
         "Limpa" todo conteúdo da lista.
     */
-    void clear() { // limpar lista
+    void clear() {  // limpar lista
         while (!empty()) {
             pop_front();
         }
-        /*if (size_ > 0) {
-            std::cout << "--- 1 --- \n";
+        if (size_ > 0) {
             Node* atual = head;
             Node* apagar;
             while (atual -> next() != nullptr) {
-                std::cout << "--- 2 --- \n";
                 apagar = atual;
                 atual = atual -> next();
                 delete apagar;
@@ -58,30 +55,28 @@ public:
                 delete atual;
                 size_--;
             }
-        }*/
-    } 
+        }
+    }
 
     //! Função push_back
     /*!
         Insere o dado no final da lista. size_-1 indicará onde é o final da lista.
         \param data é o dado a ser inserido na lista.
     */
-    void push_back(const T& data) { // inserir no fim
+    void push_back(const T& data) {  // inserir no fim
         Node* atual = head;
         if (size_ == 0) {
             push_front(data);
         } else {
             for (int i = 0; i < size_-1; i ++) {
-                std::cout << "--- 4 no for --- \n";
                 atual = atual -> next();
             }
             /*while (atual -> next() != nullptr) {
                 std::cout << "--- 4 no while--- \n";
                 atual = atual -> next();
             }*/
-            Node* novo = new Node(data);
+            Node* novo = new Node(data, nullptr);
             atual -> next(novo);
-            novo -> next(nullptr);
             size_++;
         }
     }
@@ -91,7 +86,7 @@ public:
         Insere o dado no início da lista. 
         \param data é o dado a ser inserido.
     */
-    void push_front(const T& data) { // inserir no início
+    void push_front(const T& data) {  // inserir no início
         /*Node* novo = new Node(data);
         if (size_ == 0) {
             head = novo;
@@ -102,23 +97,21 @@ public:
             novo -> next(proximo);
             head = novo;
         }*/
-        
         Node* proximo = head;
-        Node* novo = new Node(data);
-        novo -> next(proximo);
+        Node* novo = new Node(data, proximo);
         head = novo;
         size_++;
     }
-    
+
     //! Função insert
     /*!
         Insere o dado em uma posição a ser escolhida por parâmetro.
         \param data é o dado a ser inserido.
         \param index é a posição onde o dado será inserido.
     */
-    void insert(const T& data, std::size_t index) { // inserir na posição
+    void insert(const T& data, std::size_t index) {  // inserir na posição
         if (index > size_ || index < 0) {
-            throw std::out_of_range("Posição inválida!!");   
+            throw std::out_of_range("Posição inválida!!");
         }
         if (index == 0) {
             push_front(data);
@@ -126,20 +119,18 @@ public:
             Node* atual = head;
             Node* anterior = nullptr;
             for (int i = 0; i < size_; i ++) {
-                std::cout << "--- 5 --- \n";
                 if (i == index) {
-                    Node* novo = new Node(data);
+                    Node* novo = new Node(data, atual);
                     anterior -> next(novo);
-                    novo -> next(atual);
                     size_++;
-                    break;
+                    return;
                 } else {
                     anterior = atual;
                     atual = atual -> next();
                 }
             }
         }
-    }  
+    }
 
     //! Função insert_sorted
     /*!
@@ -148,35 +139,31 @@ public:
         será inserido no final da lista.
         \param data é o dado a ser inserido.
     */
-    void insert_sorted(const T& data) { // inserir em ordem
+    void insert_sorted(const T& data) {  // inserir em ordem
         if (size_ == 0) {
-            Node* novo = new Node(data);
-            novo -> next(nullptr);
+            Node* novo = new Node(data, nullptr);
             head = novo;
             size_++;
         } else {
             Node* antes = nullptr;
             Node* atual = head;
             for (int i = 0; i < size_; i ++) {
-                std::cout << "--- 6 --- \n";
                 if (i == 0 && head -> data() > data) {
                     push_front(data);
-                    break;
+                    return;
                 }
                 if (atual -> data() > data) {
-                    Node* novo = new Node(data);
-                    novo -> next(atual);
+                    Node* novo = new Node(data, atual);
                     antes -> next(novo);
                     size_++;
-                    break;    
+                    return;
                 } else {
                     if (i == size_-1) {
-                        Node* novo = new Node(data);
-                        novo -> next(nullptr);
+                        Node* novo = new Node(data, nullptr);
                         atual -> next(novo);
                         size_++;
-                        break;
-                    }    
+                        return;
+                    }
                 }
                 antes = atual;
                 atual = atual -> next();
@@ -189,14 +176,12 @@ public:
         Função que retorna o elemento que está em tal posição da lista.
         \param index é o índice da posição da lista.
     */
-    T& at(std::size_t index) { // acessar um elemento na posição index
+    T& at(std::size_t index) {  // acessar um elemento na posição index
         if (index >= size_) {
             throw std::out_of_range("Índice inválido!");
         }
         Node* atual = head;
         for (int i = 0; i < size_; i ++) {
-            //std::cout << "--- 7 --- \n";
-            std::cout << "loop \n";
             if (i == index) {
                 return atual -> data();
             } else {
@@ -212,7 +197,7 @@ public:
         \param index é o índice do dado a ser tirado e retornado.
         \return dado retirado da lista.
     */
-    T pop(std::size_t index) { // retirar da posição
+    T pop(std::size_t index) {  // retirar da posição
         if (index > size_-1 || index < 0) {
             throw std::out_of_range("Índice inválido!");
         }
@@ -221,7 +206,6 @@ public:
         Node* proximo = nullptr;
         T dado;
         for (int i = 0; i < size_-1; i ++) {
-            //std::cout << "--- 7 --- \n";
             if (i == index) {
                 dado = atual -> data();
                 anterior -> next(proximo);
@@ -234,8 +218,7 @@ public:
                 proximo = atual -> next();
             }
         }
-        
-        return dado;   
+        return dado;
     }
 
     //! Função pop_back
@@ -243,28 +226,26 @@ public:
         Função que retira o dado do final da lista e o retorna.
         \return dado retirado do final da lista
     */
-    T pop_back() { // retirar do fim
-        T dado;            
+    T pop_back() {  // retirar do fim
+        T dado;
         Node* atual = head;
         Node* anterior = nullptr;
         if (empty()) {
             throw std::out_of_range("Lista vazia!");
-        } else {
-            for (int i = 0; i < size_; i ++) {
-               // std::cout << "--- 8 --- \n";
-                if (i == size_-1) {
-                    if (i != 0) {
-                        anterior -> next(nullptr);    
-                    }
-                    dado = atual -> data();
-                    delete atual;   
-                } else {
-                    anterior = atual;
-                    atual = atual -> next();
-                }
-            }
-            size_--;
         }
+        for (int i = 0; i < size_; i ++) {
+            if (i == size_-1) {
+                if (i != 0) {
+                    anterior -> next(nullptr);
+                }
+                dado = atual -> data();
+                delete atual;
+            } else {
+                anterior = atual;
+                atual = atual -> next();
+            }
+        }
+        size_--;
         return dado;
     }
 
@@ -273,17 +254,15 @@ public:
         Função que retira o dado do início da lista e o retorna
         \return dado do início da lista.
     */
-    T pop_front() { // retirar do início
+    T pop_front() {  // retirar do início
         if (empty()) {
             throw std::out_of_range("Lista vazia!");
         } else {
             Node* apagar = head;
             T dado;
-            if (size_ > 1) {
-                head = head -> next();    
-            }
+            head = head -> next();
             dado = apagar -> data();
-            delete apagar;
+            delete (apagar);
             size_--;
             return dado;
         }
@@ -294,26 +273,23 @@ public:
         Função que remove o dado da lista que foi passado por parâmetro.
         \param data é o dado a ser removido
     */
-    void remove(const T& data) { // remover específico
+    void remove(const T& data) {  // remover específico
         if (empty()) {
             throw std::out_of_range("Lista vazia!");
-        } else {
-            Node* anterior = nullptr;
-            Node* atual = head;
-            Node* proximo = head -> next();
-            while (atual -> next() != nullptr) {
-                //std::cout << "--- 8 --- \n";
-                if (atual -> data() == data) {
-                    head = proximo;
-                    delete atual;
-                    anterior -> next(proximo);
-                    size_--;
-                    break;
-                }
-                anterior = atual;
-                atual = atual -> next();
-                proximo = atual -> next();
+        }
+        Node* anterior = nullptr;
+        Node* atual = head;
+        Node* proximo = head -> next();
+        while (atual -> next() != nullptr) {
+            if (atual -> data() == data) {
+                delete atual;
+                anterior -> next(proximo);
+                size_--;
+                return;
             }
+            anterior = atual;
+            atual = atual -> next();
+            proximo = atual -> next();
         }
     }
 
@@ -322,7 +298,7 @@ public:
         Função que verifica se a lista está vazia.
         \return true se a lista está vazia.
     */
-    bool empty() const { // lista vazia
+    bool empty() const {  // lista vazia
         if (size_ == 0) {
             return true;
         } else {
@@ -336,13 +312,13 @@ public:
         \param data é o dado a ser verificado.
         \return true se o dado estiver na lista e falso caso contrário.
     */
-    bool contains(const T& data) const { // contém
+    bool contains(const T& data) const {  // contém
         Node* atual = head;
         while (atual -> next() != nullptr) {
             if (atual -> data() == data) {
                 return true;
             } else {
-                atual = atual -> next();    
+                atual = atual -> next();
             }
         }
         return false;
@@ -354,9 +330,9 @@ public:
         \param data é o dado a ser procurado na lista.
         \return índice do elemento encontrado. Caso o elemento não está na lista, o valor retornado é size_.
     */
-    std::size_t find(const T& data) const { // posição do dado
+    std::size_t find(const T& data) const {  // posição do dado
         Node* atual = head;
-        for (int i = 0; i < size_; i ++) {    
+        for (int i = 0; i < size_; i ++) {
             if (atual -> data() == data) {
                 return i;
             } else {
@@ -370,14 +346,14 @@ public:
     /*!
         Função que retorna o tamanho da lista.
     */
-    std::size_t size() const { // tamanho da lista
+    std::size_t size() const {  // tamanho da lista
         return size_;
     }
 
-private:
-    class Node { // Elemento
-    public:
-        Node(const T& data):
+ private:
+    class Node {  // Elemento
+     public:
+        explicit Node(const T& data):
             data_{data}
         {}
 
@@ -386,32 +362,32 @@ private:
             next_{next}
         {}
 
-        T& data() { // getter: dado
+        T& data() {  // getter: dado
             return data_;
         }
 
-        const T& data() const { // getter const: dado
+        const T& data() const {  // getter const: dado
             return data_;
         }
 
-        Node* next() { // getter: próximo
+        Node* next() {  // getter: próximo
             return next_;
         }
 
-        const Node* next() const { // getter const: próximo
+        const Node* next() const {  // getter const: próximo
             return next_;
         }
 
-        void next(Node* node) { // setter: próximo
+        void next(Node* node) {  // setter: próximo
             next_ = node;
         }
 
-    private:
+     private:
         T data_;
         Node* next_{nullptr};
     };
 
-    Node* end() { // último nodo da lista
+    Node* end() {  // último nodo da lista
         auto it = head;
         for (auto i = 1u; i < size(); ++i) {
             it = it->next();
@@ -422,5 +398,5 @@ private:
     Node* head{nullptr};
     std::size_t size_{0u};
 };
-}
+}  // namespace structures
 #endif
