b4ed20ab0b06d87a7522324ff890b7b0c5c9fd2e
danielboso <bosodaniel@hotmail.com>
2016-09-26 16:54:54 -0300

    adição de métodos e atributos

--- Estrutura de Dados/Trabalho I/cliente.h
+++ Estrutura de Dados/Trabalho I/cliente.h
@@ -1,8 +1,30 @@
 
-class cliente {
-	
+class Cliente {
+
 public:
-	cliente();
-	~cliente();
-	
+
+	bool menorFila;
+	bool cartao;
+	int totalCompras;
+	int valorCompras;
+	int tempoDeChegada = 0;
+	int tempoSaida = 0;
+
+
+	Cliente(bool menorFila, bool cartao, int tempoDeChegada) {
+		this->menorFila = menorFila;
+		this->cartao = cartao;
+		this->tempoDeChegada = tempoDeChegada;
+	}
+
+	~Cliente();
+
+	Carrinho carrinho = new Carrinho();
+	totalCompras = carrinho->size();
+	valorCompras = carrinho->totalCompra();	
+
+	int setTempoSaida(int tempoSaida) {
+		this->tempoSaida = tempoSaida;
+	}
+	//
 };
