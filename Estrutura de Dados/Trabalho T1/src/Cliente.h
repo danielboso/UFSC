493a562cd04ae15aedb50a9176333924d28c809a
Adan Gomes <AdanDuM@users.noreply.github.com>
2016-09-27 22:28:59 -0300

    Atualização de Cliente.h
    Foi alterado a estrutura e estilo da classe.

--- Estrutura de Dados/Trabalho T1/src/Cliente.h
+++ Estrutura de Dados/Trabalho T1/src/Cliente.h
@@ -1,35 +1,27 @@
+//  !	Classe Cliente.
+/*	!
+ *  \Copyright (C) 2016  Adan Pereira Gomes and Daniel Boso
+ *  \Released under the GNU General Public License 2.0
+ */
+
+#ifndef SUPERMERCADO_CLIENTE_H_
+#define SUPERMERCADO_CLIENTE_H_
+
+namespace supermercado {
 
 class Cliente {
-
 public:
-
-	bool menorFila;
-	bool cartao;
-	int totalCompras;
-	int valorCompras;
-	int tempoDeChegada;
-	int tempoSaida;
-	Carrinho carrinho;
-
-
-	Cliente(bool menorFila, bool cartao, int tempoDeChegada) {
-		this->menorFila = menorFila;
-		this->cartao = cartao;
-		this->totalCompras = 0;
-		this->valorCompras = 0;
-		this->tempoDeChegada = tempoDeChegada;
-		this->tempoSaida = 0;
-		this->carrinho = new Carrinho<int>();
+	Cliente(bool cheque, int itens, int total) :
+		cheque_{ cheque }, itens_{ itens }, total_{ total } {
 	}
 
 	~Cliente();
-
-	
-	totalCompras = carrinho->size();
-	valorCompras = carrinho->totalCompra();	
-
-	int setTempoSaida(int tempoSaida) {
-		this->tempoSaida = tempoSaida;
-	}
-	//
+private:
+	bool cheque_;
+	int itens_;
+	int total_;
 };
+
+}
+
+#endif
