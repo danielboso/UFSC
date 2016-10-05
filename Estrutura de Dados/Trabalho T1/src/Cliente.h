ea203aa3587b63f7f3b9751049cbfee1707fddd1
Adan Gomes <AdanDuM@users.noreply.github.com>
2016-10-04 22:19:28 -0300

    Adicionados novos métodos e variáveis.

--- Estrutura de Dados/Trabalho T1/src/Cliente.h
+++ Estrutura de Dados/Trabalho T1/src/Cliente.h
@@ -11,17 +11,38 @@ namespace supermercado {
 
 class Cliente {
 public:
-	Cliente(bool cartao, int itens, int total) :
-		cartao_{ cartao }, itens_{ itens }, total_{ total } {
-	}
+	Cliente();
+
+	Cliente(int);
 
 	~Cliente();
+
+	const int tempo_chegada() const;
+
+	void tempo_chegada(int);
+
+	const int tempo_saida() const;
+
+	void tempo_saida(int);
+
 private:
-	bool cartao_;
-	int itens_;
-	int total_;
+	void gera_quantidade_itens();
+
+	void gera_valor_total();
+
+	// TIPO busca menor fila - ou com menos produtos
+	bool cartao_{false};  // 20% de chance
+	int quantidade_itens_{0};  // total de compras entre 2 e 100
+	int valor_total_{0};  // valor total de compras, cada item custa de 1 a 90, o valor de cada item é gerado aleatoriamente por vez
+	int tempo_chegada_{0};
+	int tempo_saida_{0};
+
+	//___________________________________
+	// se o cliente não encontrar fila de caixa com menos de 10 clientes ele cai fora
+	// ou seja, dinheiro que vai deixar de ser ganho x3
+	//____________________________________________
 };
 
-}
+}  // namespace supermercado
 
 #endif
