bd4ec4b462259bb2e77d873c9b9f62446d1e0013
danielboso <bosodaniel@hotmail.com>
2016-10-05 22:45:55 -0300

    adição de métodos

---
+++ Estrutura de Dados/Trabalho T1/src/Caixa.cpp
@@ -0,0 +1,44 @@
+//  !	Classe Caixa.
+/*	!
+ *  \Copyright (C) 2016  Adan Pereira Gomes and Daniel Boso
+ *  \Released under the GNU General Public License 2.0
+ */
+#include "Caixa.h"
+
+namespace supermercado {
+
+Caixa::Caixa() {
+	//char identificador[100];
+}
+
+Caixa::~Caixa() {}
+
+void Caixa::adicionaNumeroClientesNaFila() {
+	numeroClientesFila_++;
+}
+
+void Caixa::retiraNumeroClientesNaFila() {
+	numeroClientesFila_--;
+}
+
+void Caixa::adicionaNumeroClientesAtendidos() {
+	numeroClientesAtendidos_++;
+}
+
+void Caixa::calculaTempoMedioEspera(int tempo) {
+	tempoMedioDeEspera_ = (tempoMedioDeEspera_ + tempo)/2;
+}
+
+void Caixa::calculaFaturamentoMedio(int valor) {
+	faturamentoMedio_ = (faturamentoMedio_ + valor)/2;
+}
+
+void Caixa::calculaLucroCaixa(int tempoSimulacaoHoras) {
+	lucroDoCaixa_ = faturamento_ - (720/tempoSimulacaoHoras); // faturamento - ((30dias * 24horas)/simulaçãoHoras)
+}
+
+void Caixa::adicionaFaturamento(int faturamento) {
+	faturamento_ += faturamento;
+}
+
+}
