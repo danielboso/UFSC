5bf9e03fad074ff4a838b99d68c4cf2783ddd024
danielboso <bosodaniel@hotmail.com>
2016-10-07 00:34:54 -0300

    adicionado instancia de Visao e saída de usuário

--- Estrutura de Dados/Trabalho T1/src/Main.cpp
+++ Estrutura de Dados/Trabalho T1/src/Main.cpp
@@ -4,9 +4,17 @@
  *  \Released under the GNU General Public License 2.0
  */
 
+#include "visao/Visao.h"
+
 class Main {
 
 	int main(int argc, char **argv) {
+
+		supermercado::Visao visao = new supermercado::Visao();
+
+		visao.saidaMensagemPerguntaFormaDeExecucao();
+
+		//int numeroCaixas, int tempoMediaChegadaClientes, int tempoSimulacaoHoras
 
 	}
 };
