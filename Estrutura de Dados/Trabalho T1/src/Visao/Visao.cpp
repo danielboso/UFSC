c3f62000e14237f60238dd6f0c11a6ec050e41cd
danielboso <bosodaniel@hotmail.com>
2016-10-06 23:02:01 -0300

    Visao

---
+++ Estrutura de Dados/Trabalho T1/src/Visao/Visao.cpp
@@ -0,0 +1,14 @@
+//  !	Classe Visao.
+/*	!
+ *  \Copyright (C) 2016  Adan Pereira Gomes and Daniel Boso
+ *  \Released under the GNU General Public License 2.0
+ */
+#include "Visao.h"
+
+namespace supermercado {
+
+Visao::Visao() {}
+
+Visao::~Visao() {}
+
+} /* namespace supermercado */
