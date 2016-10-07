b9e031a2b117097e1b939fccc476b7119a1d959d
danielboso <bosodaniel@hotmail.com>
2016-10-06 23:04:59 -0300

    Main

---
+++ Estrutura de Dados/Trabalho T1/src/Main.cpp
@@ -0,0 +1,12 @@
+//  !	Classe Main.
+/*	!
+ *  \Copyright (C) 2016  Adan Pereira Gomes and Daniel Boso
+ *  \Released under the GNU General Public License 2.0
+ */
+
+class Main {
+
+	int main(int argc, char **argv) {
+
+	}
+};
