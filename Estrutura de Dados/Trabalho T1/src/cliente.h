90cc47c8d2ddd1a3b2c19149629645344da94c20
Adan Gomes <AdanDuM@users.noreply.github.com>
2016-10-09 11:58:57 -0300

    Adicionado metodos
    Modificado para snake_case

--- Estrutura de Dados/Trabalho T1/src/cliente.h
+++ Estrutura de Dados/Trabalho T1/src/cliente.h
@@ -1,4 +1,4 @@
-//!	Classe Cliente.
+//! Classe Cliente.
 /*!
  *  \Copyright (C) 2016  Adan Pereira Gomes and Daniel Boso
  *  \Released under the GNU General Public License 2.0
@@ -11,13 +11,13 @@
 
 namespace supermercado {
 
-//!	Classe Cliente
+//! Classe Cliente
 /*!	Objeto responsavel por contabilizar as compras que foram feitas no supermercado em um dado tempo.
  */
 class Cliente {
 public:
 	//! Construtor Padrão.
-	/*!	Usado para criar o objeto com valores padrão.	*/
+	/*! Usado para criar o objeto com valores padrão.	*/
 	Cliente();
 
 	//! Construtor com parametros.
@@ -28,81 +28,102 @@ public:
 	/*!	Destroi os dados armazenados na memoria e devolve a memória ao SO.	*/
 	~Cliente();
 
-    //! Método de sobrecarga do operator<<.
+	//! Método de sobrecarga do operator<<.
 	/*!	Adapta a classe para ser usada direto no output stream.
 	 *	\return um const ostream&.
 	 */
 	const std::ostream& operator<<(std::ostream&);
 
-    //! Função membro tempo_chegada.
+	//! Função membro cartao
+	/*	Getter da variavel cartao.
+	 *	/return um const bool.
+	 *	/sa cartao_
+	 */
+	const bool cartao() const;
+
+	//! Função membro quantidade_itens
+	/*	Getter da variavel quantidade_itens_.
+	 *	/return um const unsigned.
+	 *	/sa quantidade_itens_
+	 */
+	const unsigned quantidade_itens() const;
+
+	//! Função membro valor_total.
+	/*	Getter da variavel valor_total_.
+	 *	/return um unsigned.
+	 *	/sa valor_total_
+	 */
+	const unsigned valor_total() const;
+
+	//! Função membro tempo_chegada.hegada.
 	/*!	Getter da variavel tempo_chegada_.
-	 *	\return um const int.
+	 *	\return um const unsigned.
 	 *	\sa tempo_chegada(int), tempo_chegada_
 	 */
-	const int tempo_chegada() const;
+	const unsigned tempo_chegada() const;
 
-    //! Função membro tempo_chegada com parametro.
+	//! Função membro tempo_chegada com parametro.
 	/*!	Setter da variavel tempo_chegada_.
-	 * 	\param um int que indica o tempo de chegada.
+	 * 	\param um unsigned que indica o tempo de chegada.
 	 *	\sa tempo_chegada(), tempo_chegada_
 	 */
-	void tempo_chegada(int);
+	void tempo_chegada(unsigned);
 
-    //! Função membro tempo_saida.
+	//! Função membro tempo_sai.
 	/*!	Getter da variavel tempo_saida_.
-	 *	\return um const int.
+	 *	\return um const unsigned.
 	 *	\sa tempo_saida(int), tempo_saida_
 	 */
-	const int tempo_saida() const;
+	const unsigned tempo_saida() const;
 
-    //! Função membro tempo_saida com parametro.
+	//! Função membro tempo_saida com parametro.
 	/*!	Setter da variavel tempo_saida_.
-	 * 	\param int que indica i tempo de saida.
+	 * 	\param unsigned que indica i tempo de saida.
 	 *	\sa tempo_saida(), tempo_saida_
 	 */
-	void tempo_saida(int);
+	void tempo_saida(unsigned);
 
 private:
-    //! Função membro gera_cartao.
+	//! Função membro gera_cartao.
 	/*!	Responsavel por decidir se Cliente usará cartão ou não.
 	 *	\sa cartao_
 	 */
 	void gera_cartao();
 
-    //! Função membro gera_quantidade_itens.
+	//! Função membro gera_quantidade_itens.
 	/*!	Responsavel por sortear a quantidade de itens possuidos por Cliente.
 	 *	\sa quantidade_itens_
 	 */
 	void gera_quantidade_itens();
 
-    //! Função membro gera_valor_total.
+	//! Função membro gera_valor_total.
 	/*!	Responsavel por sortear e somar os valores dos itens possuidos por Cliente.
 	 *	\sa valor_total_
 	 */
 	void gera_valor_total();
 
-    //! Variavel privada cartao_ .
+	//! Variavel privada cartao_ .
 	/*!	Indica se o Cliente usará ou não cartão no pagamento.	*/
 	bool cartao_{false};
 
-    //! Variavel privada quantidade_itens_ .
+	//! Variavel privada quantidade_itens_ .
 	/*!	Indica o quantidade de produtos possuidos por Cliente.	*/
-	int quantidade_itens_{0};
+	unsigned quantidade_itens_{0};
 
-    //! Variavel privada tempo_saida_ .
+	//! Variavel privada tempo_saida_ .
 	/*!	Indica o valor total dos produtos possuidos por Cliente.	*/
-	int valor_total_{0};
+	unsigned valor_total_{0};
 
-    //! Variavel privada tempo_chegada_ .
+	//! Variavel privada tempo_chegada_ .
 	/*!	Indica o tempo em que Cliente chegou a Fila.	*/
-	int tempo_chegada_{0};
+	unsigned tempo_chegada_{0};
 
-    //! Variavel privada tempo_saida_ .
+	//! Variavel privada tempo_saida_ .
 	/*!	Indica o tempo em que Cliente deixará a Fila.	*/
-	int tempo_saida_{0};
+	unsigned tempo_saida_{0};
 
 };
 
-}
+}  // namespace supermercado
 
 #endif
