fe8559e892501cc9412f4a06ee5395710fef947e
Adan Gomes <AdanDuM@users.noreply.github.com>
2016-10-10 22:44:48 -0300

    Alterados tipos das variaveis

--- Estrutura de Dados/Trabalho T1/src/caixa.cpp
+++ Estrutura de Dados/Trabalho T1/src/caixa.cpp
@@ -8,65 +8,65 @@
 
 namespace supermercado {
 
-Caixa::Caixa(char* identificador, unsigned eficiencia, unsigned salario) :
+Caixa::Caixa(std::string identificador, unsigned eficiencia, unsigned salario) :
 		identificador_{identificador},
 		eficiencia_{eficiencia},
 		salario_{salario}
 {
-	clientes_ = new structures::LinkedQueue<Cliente>();
+	clientes_ = new structures::LinkedQueue<Cliente*>();
 }
 
 Caixa::~Caixa() {}
 
-void Caixa::recebe_cliente(Cliente& cliente) {
+void Caixa::recebe_cliente(Cliente* cliente) {
 	clientes_->enqueue(cliente);
-	produtos_pendentes_ += cliente.quantidade_itens();
+	produtos_pendentes_ += cliente->quantidade_itens();
 	processa_compras(cliente);
 }
 
-void Caixa::retira_cliente(unsigned& tempo) {
-	if (clientes_->front().tempo_saida() == tempo) {
-		produtos_pendentes_ -= clientes_->front().quantidade_itens();
-		faturamento_total_ += clientes_->front().valor_total();
-		faturamento_medio(clientes_->front().valor_total());
-		tempo_espera_medio(clientes_->front().tempo_saida() - clientes_->front().tempo_chegada());
+void Caixa::retira_cliente(const unsigned& tempo) {
+	if (clientes_->front()->tempo_saida() == tempo) {
+		produtos_pendentes_ -= clientes_->front()->quantidade_itens();
+		faturamento_total_ += clientes_->front()->valor_total();
+		faturamento_medio(clientes_->front()->valor_total());
+		tempo_espera_medio(clientes_->front()->tempo_saida() - clientes_->front()->tempo_chegada());
 		++clientes_atendidos_;
 		clientes_->dequeue();
 	}
 }
 
-void Caixa::processa_compras(Cliente& cliente) {
+void Caixa::processa_compras(Cliente* cliente) {
 	auto tempo = 0u;
 	switch (eficiencia_) {
 		case 1:
-			tempo += cliente.quantidade_itens();
-			if (cliente.cartao()) {
+			tempo += cliente->quantidade_itens();
+			if (cliente->cartao()) {
 				tempo += 10u;
 			}
-			cliente.tempo_saida(cliente.tempo_chegada() + tempo);
+			cliente->tempo_saida(cliente->tempo_chegada() + tempo);
 			break;
 		case 2:
-			tempo += cliente.quantidade_itens() * 2;
-			if (cliente.cartao()) {
+			tempo += cliente->quantidade_itens() * 2;
+			if (cliente->cartao()) {
 				tempo += 25u;
 			}
-			cliente.tempo_saida(cliente.tempo_chegada() + tempo);
+			cliente->tempo_saida(cliente->tempo_chegada() + tempo);
 			break;
 		case 3:
-			tempo += cliente.quantidade_itens() * 4;
-			if (cliente.cartao()) {
+			tempo += cliente->quantidade_itens() * 4;
+			if (cliente->cartao()) {
 				tempo += 60u;
 			}
-			cliente.tempo_saida(cliente.tempo_chegada() + tempo);
+			cliente->tempo_saida(cliente->tempo_chegada() + tempo);
 			break;
 	}
 }
 
 const std::ostream& Caixa::operator<<(std::ostream& saida) const {
 	return saida << "Caixa (" <<
-			"\n\tidentificador: " 			<< identificador_ 			<<
-			"\n\teficiencia: " 					<< eficiencia_ 					<<
-			"\n\tsalario: " 						<< salario_ 						<<
+			"\n\tidentificador: " 			<< identificador_ 	<<
+			"\n\teficiencia: " 					<< eficiencia_ 	<<
+			"\n\tsalario: " 						<< salario_ <<
 			"\n\tprodutos_pendentes: " 	<< produtos_pendentes_ 	<<
 			"\n\tclientes_atendidos: " 	<< clientes_atendidos_ 	<<
 			"\n\tfaturamento_medio: " 	<< faturamento_medio_ 	<<
@@ -75,7 +75,7 @@ const std::ostream& Caixa::operator<<(std::ostream& saida) const {
 			"\n)";
 }
 
-const char* Caixa::identificador() const { return identificador_; }
+const std::string Caixa::identificador() const { return identificador_; }
 
 const unsigned Caixa::eficiencia() const { return eficiencia_; }
 
@@ -95,9 +95,9 @@ void Caixa::faturamento_medio(int valor) {
 	}
 }
 
-const int Caixa::faturamento_medio() const { return faturamento_medio_; }
+const double Caixa::faturamento_medio() const { return faturamento_medio_; }
 
-const int Caixa::faturamento_total() const { return faturamento_total_; }
+const double Caixa::faturamento_total() const { return faturamento_total_; }
 
 void Caixa::tempo_espera_medio(unsigned tempo) {
 	if (tempo_espera_medio_ == 0u) {
