89e55846debd68ba27725771c5fd519d7dca8fd2
Adan Gomes <AdanDuM@users.noreply.github.com>
2016-10-12 11:20:28 -0300

    Adicionado metodo de inserção de caixa

--- Estrutura de Dados/Trabalho T1/src/super_mercado.cpp
+++ Estrutura de Dados/Trabalho T1/src/super_mercado.cpp
@@ -8,10 +8,6 @@
 
 namespace supermercado {
 
-SuperMercado::SuperMercado() {
-
-}
-
 SuperMercado::SuperMercado(std::string nome_mercado, unsigned tempo_simulacao, unsigned tempo_medio_chegada, unsigned total_caixas) :
 		nome_mercado_{nome_mercado},
 		tempo_simulacao_{tempo_simulacao},
@@ -33,14 +29,19 @@ void SuperMercado::inicia_simulacao() {
 	}
 }
 
+void SuperMercado::adiciona_caixa(std::string identificador , unsigned eficiencia, unsigned salario) {
+	Caixa caixa = new Caixa(identificador, eficiencia, salario);
+	caixas_.push_front(&caixa);
+}
+
 void SuperMercado::atualiza_caixas() {
-	for (auto i = 0u; i != caixas_->size(); ++i) {
-		caixas_->at(i)->retira_cliente(relogio_);
+	for (auto i = 0u; i != caixas_.size(); ++i) {
+		delete caixas_.at(i)->retira_cliente(relogio_);
 	}
 }
 
 void SuperMercado::gera_cliente() {
-	Cliente* cliente = new Cliente();
+	Cliente* cliente = new Cliente(relogio_);
 	if (lugar_fila()) {
 		if (cliente->tipo() == MENOS_PRODUTOS) {
 			menos_produtos()->recebe_cliente(cliente);
@@ -59,28 +60,28 @@ void SuperMercado::calcula_perda(Cliente* cliente) {
 }
 
 Caixa* SuperMercado::menos_produtos() {
-	Caixa* caixa = caixas_->at(0u);
-	for (auto i = 0u; i != caixas_->size(); ++i) {
-		if(caixas_->at(i)->produtos_pendentes() < caixa->produtos_pendentes()) {
-			caixa = caixas_->at(i);
+	Caixa* caixa = caixas_.at(0u);
+	for (auto i = 0u; i != caixas_.size(); ++i) {
+		if(caixas_.at(i)->produtos_pendentes() < caixa->produtos_pendentes()) {
+			caixa = caixas_.at(i);
 		}
 	}
 	return caixa;
 }
 
 Caixa* SuperMercado::menor_fila() {
-	Caixa* caixa = caixas_->at(0u);
-	for (auto i = 0u; i != caixas_->size(); ++i) {
-		if(caixas_->at(i)->clientes_fila() < caixa->clientes_fila()) {
-			caixa = caixas_->at(i);
+	Caixa* caixa = caixas_.at(0u);
+	for (auto i = 0u; i != caixas_.size(); ++i) {
+		if(caixas_.at(i)->clientes_fila() < caixa->clientes_fila()) {
+			caixa = caixas_.at(i);
 		}
 	}
 	return caixa;
 }
 
 bool SuperMercado::lugar_fila() {
-	for (auto i = 0u; i != caixas_->size(); ++i) {
-		if(caixas_->at(i)->clientes_fila() < 10) {
+	for (auto i = 0u; i != caixas_.size(); ++i) {
+		if(caixas_.at(i)->clientes_fila() < 10) {
 			return true;
 		}
 	}
