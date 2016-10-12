203de6a69f8cd9f96053fbaec30fa5dad7af8212
danielboso <bosodaniel@hotmail.com>
2016-10-12 09:51:33 -0300

    Main refatorado

--- Estrutura de Dados/Trabalho T1/src/main.cpp
+++ Estrutura de Dados/Trabalho T1/src/main.cpp
@@ -7,38 +7,13 @@
 #include "visao/visao.h"
 #include "super_mercado.h"
 #include "excecoes.h"
-#include <iostream>
 
 int main(int argc, char* argv[]) {
 
 	supermercado::Visao visao{};
-	supermercado::SuperMercado *supermercado;
+
 	visao.mensagem_forma_execucao();
-	std::string forma_execucao;
-	bool ler_arquivo;
-
-	bool excecao = true;
-	while (excecao) {
-		forma_execucao = visao.entrada_forma_execucao();
-		try {
-	 		if ((std::stoi(forma_execucao)) < 0 || (std::stoi(forma_execucao)) > 1) {
-	 			throw ExcecaoOpcaoInvalida();
-	 		}
-	 		excecao = false;
-	 	} catch (ExcecaoOpcaoInvalida e) {
-	 			excecao = true;
-	 			std::cout << e.what();
-	 	} catch (std::invalid_argument e) {
-			excecao = true;
-			std::cout << "Argumento Inválido!\n";
-		}
-	}
-
-	if (std::stoi(forma_execucao) == 1) {
-		ler_arquivo = true;
-	} else {
-		ler_arquivo = false;
-	}
+	bool ler_arquivo = visao.entrada_forma_execucao();
 
 	std::string nome_supermercado;
 	unsigned tempo_simulacao;
@@ -48,79 +23,22 @@ int main(int argc, char* argv[]) {
 	if (ler_arquivo) {
 		// Gera supermercado com leitura pelo arquivo
 	} else {
-		visao.mensagem_nome_do_supermercado();
 
-		bool excecao_nome_supermercado = true;
-		while(excecao_nome_supermercado) {
-			try {
-				excecao_nome_supermercado = false;
-				nome_supermercado = visao.entrada_nome_do_supermercado();
-			} catch(std::invalid_argument e) {
-					excecao_nome_supermercado = true;
-					std::cout << "Argumento Inválido";
-			}
-		}
+		visao.mensagem_nome_do_supermercado();
+		nome_supermercado = visao.entrada_nome_do_supermercado();
 
 		visao.mensagem_tempo_simulacao();
-
-		bool excecao_tempo_simulacao = true;
-		while(excecao_tempo_simulacao) {
-			try {
-				excecao_tempo_simulacao = false;
-				tempo_simulacao = std::stoul(visao.entrada_tempo_simulacao());
-				if (tempo_simulacao <= 0) {
-					throw ExcecaoEntradaInvalida();
-				}
-			} catch (ExcecaoEntradaInvalida e) {
-				excecao_tempo_simulacao = true;
-				std::cout << "Entrada Inválida!";
-			} catch (std::invalid_argument e) {
-				excecao_tempo_simulacao = true;
-				std::cout << "Entrada Inválida!";
-			}
-		}
+		tempo_simulacao = visao.entrada_tempo_simulacao();
 
 		visao.mensagem_tempo_medio_chegada();
-
-		bool excecao_tempo_medio_chegada = true;
-		while(excecao_tempo_medio_chegada) {
-			try {
-				excecao_tempo_medio_chegada = false;
-				tempo_medio_chegada = std::stoul(visao.entrada_tempo_medio_chegada());
-				if (tempo_medio_chegada <= 0) {
-					throw ExcecaoEntradaInvalida();
-				}
-			} catch(ExcecaoEntradaInvalida e) {
-					excecao_tempo_medio_chegada = true;
-					std::cout << "Entrada Inválida!";
-			} catch(std::invalid_argument e) {
-					excecao_tempo_medio_chegada = true;
-					std::cout << "Entrada Inválida!";
-			}
-		}
+		tempo_medio_chegada = visao.entrada_tempo_medio_chegada();
 
 		visao.mensagem_total_caixas();
+		total_caixas = visao.entrada_total_caixas();
 
-		bool excecao_total_caixas = true;
-		while(excecao_total_caixas) {
-			try {
-				excecao_total_caixas = false;
-				total_caixas = std::stoul(visao.entrada_total_caixas());
-				if (total_caixas <= 0) {
-					throw ExcecaoEntradaInvalida();
-				}
-			} catch(ExcecaoEntradaInvalida e) {
-					excecao_total_caixas = true;
-					std::cout << "Entrada Inválida!";
-			} catch(std::invalid_argument e) {
-					excecao_total_caixas = true;
-					std::cout << "Entrada Inválida!";
-			}
-		}
-
-		supermercado = new supermercado::SuperMercado(nome_supermercado, tempo_simulacao, tempo_medio_chegada, total_caixas); // Gera supermercado com informações dadas pelo usuário
-		supermercado->inicia_simulacao();
 	}
+	supermercado::SuperMercado supermercado = new supermercado::SuperMercado(nome_supermercado, tempo_simulacao, tempo_medio_chegada, total_caixas); // Gera supermercado com informações dadas pelo usuário
 
+	// Instancia Caixas
 	return 0;
 }
