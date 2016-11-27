fa1212749406d5dc9cf570e20bbfecee7dc589b8
danielboso <bosodaniel@hotmail.com>
2016-11-27 16:02:57 -0200

    test

---
+++ Estrutura de Dados/Trabalho T2/teste.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+  char a = 'a';
+  char b = 'b';
+  char c = 'c';
+  char d = 'd';
+  char f = 'f';
+
+  std::cout << "a em decimal: ";
+  int ia = a;
+  std::cout << ia << "\n";
+
+  std::cout << "b em decimal: ";
+  int ib = b;
+  std::cout << ib << "\n";
+
+  std::cout << "c em decimal: ";
+  int ic = c;
+  std::cout << ic << "\n";
+
+  std::cout << "d em decimal: ";
+  int id = d;
+  std::cout << id << "\n";
+
+  std::cout << "f em decimal: ";
+  int ief = f;
+  std::cout << ief << "\n";
+
+  return 0;
+}
