9800a8805c82425eea2fcc7074db68d1c89fef1f
danielboso <bosodaniel@hotmail.com>
2017-09-02 21:52:35 -0300

    adição de novos predicados

--- Paradigmas de Programação/Trabalho 1/programa.pl
+++ Paradigmas de Programação/Trabalho 1/programa.pl
@@ -1,2 +1,10 @@
 
 % Linguagens L que possuem algum predecessor
+lingcompre(L) :- predecessora(L, _).
+
+% Linguagens L que possuem algum predecessor, e também são predecessoras de outra
+% linguagem
+lingprecompre(L) :- predecessora(L, _), predecessora(_, L).
+
+% Linguagens precessoras Lp de linguagens desenvolvidas em um determinado ano A
+lingpreano(Lp, A) :- predecessora(X, Lp), linguagem(X, A).
