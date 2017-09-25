5ea96e2a0377750289ce8bb74221dabdc6a546f9
danielboso <bosodaniel@hotmail.com>
2017-09-25 18:15:37 -0300

    .

--- Programação Concorrente/2017.2/sudokun/base/sudokun.c
+++ Programação Concorrente/2017.2/sudokun/base/sudokun.c
@@ -1,82 +1,238 @@
-		#include "sudokun.h"
+#include "sudokun.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 
 #define MAX_n 8
 
+int size;
+uint16_t size_squared;
+uint16_t board_size;
+
+uint16_t clues = 0;
+int solutions = 0;
+
+uint16_t **constrainted_elements;
+uint16_t **constraint_matrix;
+
+int16_t *remaining_choices;
+uint16_t *choosen_constraint;
+
+
 int main (int argc, char **argv) {
-	int size;
-	assert(scanf("%d", &size) == 1);
+
+	assert(scanf("%u", &size) == 1);
 	assert (size <= MAX_n);
-	
-	int buf_size = size * size * size * size;
-	int buf[buf_size];
+	//size = 2;
 
-	for (int i = 0; i < buf_size; i++) {
-		if (scanf("%d", &buf[i]) != 1) {
-    		printf("error reading file (%d)\n", i);
-      		exit(1);
-  		}
-  	}
+	size_squared = size * size;
+	board_size = size_squared * size_squared;
 
-	print_grid(buf, size);
+	uint8_t *buffer;
+
+	buffer = read_grid();
+
+	generate_matrix();
+
+	//solve(buffer);
+
+	print_grid(buffer);
+
+	destroy_matrix();
+
+	free(buffer);
 
 	return 0;
 }
 
-int chek_row(int buf[], int row) {
+uint8_t *read_grid() {
+	uint8_t *grid_buffer;
+
+	grid_buffer = calloc(board_size, sizeof(uint8_t));
+
 	int i;
-	int begin_row = row * MAX_n;
-	int   end_row = begin_row + MAX_n;
+	for (i = 0; i < board_size; i++) {
+		if (scanf("%d", &grid_buffer[i]) != 1) {
+			printf("error reading file (%d)\n");
+			exit(1);
+		}
+	}
 
-	for(i = begin_row; i < end_row; i ++) {
-		int j;
-		for(j =  begin_row; i < end_row; j++) {
+	return grid_buffer;
+}
 
-			if ( (buf[i] == buf[j])  && (i != j) ) {
-				return 0;
+void print_grid(uint8_t buffer[]) {
+	for (int i = 0; i < size_squared; i++) {
+	for (int j = 0; j < size_squared; j++)
+		printf("%02u ", buffer[i * size_squared + j]);
+
+	printf("\n");
+	}
+}
+
+void generate_matrix() {
+	int row = 0;
+	int i, j, k;
+	int ce_size = 4 * board_size;
+	int cm_size = board_size * size_squared;
+
+	constrainted_elements = calloc(ce_size, sizeof(uint16_t*));
+	constraint_matrix = calloc(cm_size, sizeof(uint16_t*));
+
+	for (i = 0; i < ce_size; ++i) constrainted_elements[i] = calloc(size_squared, sizeof(uint16_t));
+	for (i = 0; i < cm_size; ++i) constraint_matrix[i] = calloc(4, sizeof(uint16_t));
+
+	for (i = 0; i < size_squared; ++i) {        // Para cada linha
+		for (j = 0; j < size_squared; ++j) {      // Para cada coluna
+			for (k = 0; k < size_squared; ++k) {    // Para cada numero possivel
+				printf("Linha: %03u -", row);
+				constraint_matrix[row][0] = size_squared * i + j;                                          // celula
+				constraint_matrix[row][1] = size_squared * (i/size*size + j/size) + k + board_size;   // caixa
+				constraint_matrix[row][2] = size_squared * i + k + (board_size * 2);                       // linha
+				constraint_matrix[row][3] = size_squared * j + k + (board_size * 3);                       // coluna
+				printf(" %03u", constraint_matrix[row][0]);
+				printf(" %03u", constraint_matrix[row][1]);
+				printf(" %03u", constraint_matrix[row][2]);
+				printf(" %03u\n", constraint_matrix[row][3]);
+				++row;
 			}
 		}
 	}
-	return 1;
+	printf("\n");
+
+	uint8_t temp_vector[board_size * 4];
+	uint32_t end_range;
+	int temp;
+
+	end_range = board_size * 4;
+	for (i = 0; i < end_range; ++i) {
+		temp_vector[i] = 0;
+	}
+
+	int constraint;
+	end_range = board_size * size_squared;
+	for (row = 0; row < end_range; ++row) {
+		for (constraint = 0; constraint < 4; ++constraint) {
+			temp = constraint_matrix[row][constraint];
+			constrainted_elements[temp][temp_vector[temp]++] = row;
+		}
+	}
+
+	end_range = board_size * 4;
+	for (row = 0; row < end_range; ++row) {
+		printf("Linha: %03u -", row);
+		for (constraint = 0; constraint < size_squared; ++constraint) {
+			printf(" %03u", constrainted_elements[row][constraint]);
+		}
+		printf("\n");
+	}
+
 }
 
-int check_column(int buf[], int column) {
+void destroy_matrix() {
+	int ce_size = 4 * board_size;
+	int cm_size = board_size * size_squared;
+
+	constrainted_elements = calloc(ce_size, sizeof(uint16_t*));
+	constraint_matrix = calloc(cm_size, sizeof(uint16_t*));
+
 	int i;
-	int begin_column = column;
-	int   end_column = MAX_n * column;
+	for (i = 0; i < ce_size; ++i) free(constrainted_elements[i]);
+	for (i = 0; i < cm_size; ++i) free(constraint_matrix[i]);
 
-	for(i = begin_column; i < end_column; i ++) {
-		int j;
-		for(j = begin_column; i < end_column; j ++) {
-			// Número fixo da coluna
-			int position_fixed = i * MAX_n;
+	free(constraint_matrix);
+	free(constrainted_elements);
+}
 
-			// Posições a ser verificada se position_number_fixed é igual a position_to_verify
-			int position_to_verify = i * j;
+int update(int value, int option) {  // Se option = 1 continua, se option = -1 backtracking
+	int min_high = 10, min_low = 0;
+	uint8_t row;
 
-			int number_equal = (position_fixed == position_to_verify);
+	int constraint;
+	for (constraint = 0; constraint < 4; ++constraint) {  // Para cada restrição associada a value
+		int neighbor, neighbor_constraint, constrainted = constraint_matrix[value][constraint];
+	    if (option > 0) {   // Atualiza valor
+	  		for (neighbor = 0; neighbor < size_squared; ++neighbor) {   // Para cada vizinho do numero e.g. linha, coluna, caixa
+	    		row = constrainted_elements[constrainted][neighbor];
+		        if (choosen_constraint[row]++ != 0) {
+		        	continue;
+		        }
 
-			if ( (buf[position_fixed] == buf[position_to_verify]) && number_equal) {
-				return 0;
+				for (neighbor_constraint = 0; neighbor_constraint < 4; ++neighbor_constraint) {
+					int index = constraint_matrix[row][neighbor_constraint];
+					if (--remaining_choices[index] < min_high) {
+						min_high = remaining_choices[index];
+						min_low = index;
+					}
+				}
+			}
+	    } else {          // Backtracking - Desfaz modificações
+			uint16_t *p;
+			for (neighbor = 0; neighbor < size_squared; ++size_squared) {   // Para cada vizinho do numero e.g. linha, coluna, caixa
+				row = constrainted_elements[constrainted][neighbor];
+				if (--choosen_constraint[row] != 0) {
+					continue;
+				}
+				p = constraint_matrix[row];
+				++remaining_choices[p[0]];
+				++remaining_choices[p[1]];
+				++remaining_choices[p[2]];
+				++remaining_choices[p[3]];
 			}
 		}
 	}
-	return 1;
+
+	return min_high << 16 | min_low;
 }
 
-int check_region(int buf[]) {
+void insert_clues(uint8_t buffer[]) {
+	int i, j, number;
 
+	for (i = 0; i < size_squared; ++i) {
+		for (j = 0; j < size_squared; ++j) {
+			if (buffer[i * size_squared + j] >= 1 && buffer[i * size_squared + j] <= size_squared) {  // Se o número esta dentro da faixa
+				number = buffer[i * size_squared + j] - 1;
+			} else {
+				number = -1;
+			}
+			if (number >= 0) {
+				update(i * size_squared + number, 1);
+				++clues;
+			}
+		}
+	}
 }
 
-void print_grid(int buf[], int size) {
-  int size_squared = size * size;
+void solve(uint8_t buffer[]) {
 
-  for (int i = 0; i < size_squared; i++) {
-    for (int j = 0; j < size_squared; j++)
-      printf("%d ", buf[i * size_squared + j]);
+	remaining_choices = calloc(board_size * 4, sizeof(uint16_t));
+	choosen_constraint = calloc(board_size * size_squared, sizeof(uint16_t));
 
-    printf("\n");
-  }
+	insert_clues(buffer);
+
+
+
+	int i = 0;
+	while(1) {
+		while (i < board_size - clues) {
+
+		}
+		if (i < 0) {
+			break;
+		}
+	}
+
+	free(choosen_constraint);
+	free(remaining_choices);
 }
+
+/*void *thread_solve() {
+	// recursive
+	// escolhe o menor numero possivel
+	// loop: se for valido testa e tenta colocar o numero
+	// se for invalido e for o ultimo da backtrack
+}*/
+
+/*bool valid() {
+
+}*/
