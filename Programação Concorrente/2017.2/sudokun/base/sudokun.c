e858d218e2e0c01b544e0747c32ecccb958c07c2
AdanDuM <adandum@hotmail.com>
2017-09-27 20:25:08 -0300

    Implementado valid

--- Programação Concorrente/2017.2/sudokun/base/sudokun.c
+++ Programação Concorrente/2017.2/sudokun/base/sudokun.c
@@ -130,11 +130,9 @@ void destroy_matrix() {
 }
 
 int update(int value, int option) {  // Se option = 1 continua, se option = -1 backtracking
-	int min_high = 10, min_low = 0;
+	int valid = 1;
 
 	int constraint, row;
-	for (constraint = 0; constraint < 4; ++constraint) remaining_choices[constraint_matrix[value][constraint]] += option<<7; // importante pra solve
-
 	for (constraint = 0; constraint < 4; ++constraint) {  // Para cada restrição associada a value
 		int neighbor, neighbor_constraint, constrainted = constraint_matrix[value][constraint];
 	    if (option > 0) {   // Atualiza valor
@@ -143,28 +141,14 @@ int update(int value, int option) {  // Se option = 1 continua, se option = -1 b
 		        if (choosen_constraint[row]++ != 0) {
 		        	continue;
 		        }
-
-				for (neighbor_constraint = 0; neighbor_constraint < 4; ++neighbor_constraint) {
-					int index = constraint_matrix[row][neighbor_constraint];
-					if (--remaining_choices[index] < min_high) {
-						min_high = remaining_choices[index];
-						min_low = index;
-					}
-				}
+				valid &= 1;
 			}
 	    } else {          // Desfaz modificações
-			const int *p;
 			for (neighbor = 0; neighbor < size_squared; ++neighbor) {   // Para cada vizinho do numero e.g. linha, coluna, caixa
 				row = constrainted_elements[constrainted][neighbor];
 				if (--choosen_constraint[row] != 0) {
 					continue;
 				}
-				p = constraint_matrix[row];
-				++remaining_choices[p[0]];
-				++remaining_choices[p[1]];
-				++remaining_choices[p[2]];
-				++remaining_choices[p[3]];
-			}
 		}
 	}
 
@@ -191,7 +175,7 @@ int solve(int buffer[]) {
 	int i = 0, index, option = 1;
 	int solution = 0;
 
-	int min = 10, r2 = 0, c, cand = 10<<16;
+	int min = size_squared + 1, r2 = 0, c, cand = (size_squared+1)<<16;
 
 	remaining_choices = calloc(board_size * 4, sizeof(int));
 	choosen_constraint = calloc(board_size * size_squared, sizeof(int));
@@ -201,57 +185,31 @@ int solve(int buffer[]) {
 
 	insert_clues(buffer);
 
-	int current_row[board_size];
-	int current_column[board_size];
+	// int current_row[board_size];
+	// int current_column[board_size];
+	//
+	// for (index = 0; index < board_size; ++index) {
+	// 	current_row[index] = -1;
+	// 	current_column[index] = -1;
+	// }
 
-	for (index = 0; index < board_size; ++index) {
-		current_row[index] = -1;
-		current_column[index] = -1;
-	}
+	int testoutudo[board_size*size_squared];
+	int current_row = 0;
 
 	while(1) {
-		while (i >= 0 && i < board_size - clues) {
-			if (option == 1) {
-
-				min = cand>>16;
-				current_column[i] = cand&0xffff;
-				if (min>1) {
-					for (c = 0; c < (board_size*4); ++c) { // para cada uma das restrições
-						if (remaining_choices[c] < min) { // se a restrição for menor que min, min = restrição
-							min = remaining_choices[c];
-							current_column[i] = c; // choose the top constraint - min = 9 (inicialmente) e cc[i] = c (0 inicialmente)
-							if (min <= 1) {
-								break; // this is for acceleration; slower without this line - Se min for igual a 1 ja chegou onde queria
-							}
-						}
-					}
-				}
-				if (min == 0 || min == (size_squared+1)) {
-					current_row[i--] = option = -1; // backtrack, pois sai da faixa 1~9
-				}
+		while (1) {
+			for(i = current_row; i < board_size*size_squared; ++i) { // procura primeiro numero/posicao disponivel
+				if (testoutudo[i] == 0) break;
 			}
-			c = current_column[i];
-			if (option == -1 && current_row[i] >= 0) {
-				update(constrainted_elements[c][current_row[i]], -1);
-			}
+			if (update(i, 1) == 1) {
 
-			for (r2 = current_row[i] + 1; r2 < size_squared; ++r2) {// search for the choice to make - pega o proximo numero disponivel
-				if (choosen_constraint[constrainted_elements[c][r2]] == 0) {
-					break; // found if the state equals 0
-				}
-			}
+			} else {
 
-			if (r2 < size_squared) { // pega o proximo numero disponivel e tenta
-				cand = update(constrainted_elements[c][r2], 1); // set the choice
-				current_row[i++] = r2; option = 1; // moving forward - como deu certo, a linha escolhida = linha testada e dir = 1
-			} else current_row[i--] = option = -1; // backtrack - volta pra linha anterior e backtrack
+			}
 		}
-		if (i < 0) {
-			break;
-		}
-		++solution;
-		--i;
-		option = -1;
+		// ++solution;
+		// --i;
+		// option = -1;
 	}
 
 	free(choosen_constraint);
@@ -261,12 +219,44 @@ int solve(int buffer[]) {
 }
 
 /*void *thread_solve() {
-	// recursive
 	// escolhe o menor numero possivel
+	array de posicao cada uma guardando os numeros possiveis
+	se existe numeros possiveis pode tentar ainda
+	tenta colocar um valor, caso de certo adiciona no array e prossegue
+	tenta colocar e faz o teste de forma paralela
+	ao final verifica se ta tudo certo e avança
+	//preciso saber que passei por todas as possibilidades;
+	int testoutudo[board_size*size_squared];
+	while (1) {
+		for(i = 0; i < board_size*size_squared; ++i) { // procura primeiro numero/posicao disponivel
+			if (testoutudo[i] == 0) break;
+		}
+		if (update(i, 1) == 1) {
+
+		} else {
+
+		}
+	}
+	// quando uma das threads voltar em recursão ele pode chamar mais threads com o numero da solução
 	// loop: se for valido testa e tenta colocar o numero
 	// se for invalido e for o ultimo da backtrack
 }*/
 
-/*bool valid() {
+int valid_position(int value) {
+	int valid = 1;
 
-}*/
+	int constraint, row;
+	for (constraint = 0; constraint < 4; ++constraint) {  // Para cada restrição associada a value
+		int neighbor, neighbor_constraint, constrainted = constraint_matrix[value][constraint];
+		for (neighbor = 0; neighbor < size_squared; ++neighbor) {   // Para cada vizinho do numero e.g. linha, coluna, caixa
+			row = constrainted_elements[constrainted][neighbor];
+			if (choosen_constraint[row] == 0) {
+				valid &= 1;
+			} else {
+				valid &= 0;
+			}
+		}
+	}
+
+	return valid;
+}
