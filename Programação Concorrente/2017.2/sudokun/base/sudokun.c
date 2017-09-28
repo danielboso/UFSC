8fb83b84818729844f59ab87a2a4a1df67abd358
danielboso <bosodaniel@hotmail.com>
2017-09-27 23:14:05 -0300

    .

--- Programação Concorrente/2017.2/sudokun/base/sudokun.c
+++ Programação Concorrente/2017.2/sudokun/base/sudokun.c
@@ -2,15 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <pthread.h>
 
 #define MAX_n 8
 
+// Variaveis globais do Sudoku
 int size;
 int size_squared;
 int board_size;
 
 int clues = 0;
 int solution = 0;
+int count = 0;
 
 int **constrainted_elements;
 int **constraint_matrix;
@@ -18,14 +21,45 @@ int **constraint_matrix;
 int *remaining_choices;
 int *choosen_constraint;
 
+// Variaveis das threads
+struct Range {
+  unsigned id;
+  unsigned initial_range;
+  unsigned final_range;
+};
+
+pthread_mutex_t mutex;
+
+unsigned num_threads;
+
+int least_choices;
+
+int reference_row;
+
+int vector_size;
+
+int portion;
+
+int *current_column;
 
 int main (int argc, char **argv) {
+
+	pthread_mutex_init(&mutex, NULL);
+
+    unsigned num_threads = atoi(argv[1]);
+
+	printf("NUMTHREADS: %02d\n", num_threads);
 
 	assert(scanf("%u", &size) == 1);
 	assert (size <= MAX_n);
 
 	size_squared = size * size;
 	board_size = size_squared * size_squared;
+	vector_size = board_size * 4;
+
+	portion = vector_size / num_threads;
+
+	printf("Portion: %02d\n", portion);
 
 	int *buffer;
 
@@ -37,9 +71,12 @@ int main (int argc, char **argv) {
 
 	solution = solve(buffer);
 
-	printf("Soluções: %02d", solution);
+	printf("Soluções: %02d\n", solution);
+	printf("Count: %02d\n", count);
 
 	destroy_matrix();
+
+	pthread_mutex_destroy(&mutex);
 
 	free(buffer);
 
@@ -73,7 +110,7 @@ void print_grid(int buffer[]) {
 	}
 }
 
-void generate_matrix() {
+void generate_matrix() {	// Lookup table
 	int row = 0;
 	int i, j, k;
 	int ce_size = 4 * board_size;
@@ -130,7 +167,11 @@ void destroy_matrix() {
 }
 
 int update(int value, int option) {  // Se option = 1 continua, se option = -1 backtracking
+<<<<<<< HEAD
 	int valid = 1;
+=======
+	int min_index = size_squared + 1;
+>>>>>>> f6908ebd31eb5a8e993f0d9ecb083567928e8b2e
 
 	int constraint, row;
 	for (constraint = 0; constraint < 4; ++constraint) {  // Para cada restrição associada a value
@@ -141,7 +182,17 @@ int update(int value, int option) {  // Se option = 1 continua, se option = -1 b
 		        if (choosen_constraint[row]++ != 0) {
 		        	continue;
 		        }
+<<<<<<< HEAD
 				valid &= 1;
+=======
+
+				for (neighbor_constraint = 0; neighbor_constraint < 4; ++neighbor_constraint) {
+					int index = constraint_matrix[row][neighbor_constraint];
+					if (--remaining_choices[index] < min_index) {
+						min_index = index;
+					}
+				}
+>>>>>>> f6908ebd31eb5a8e993f0d9ecb083567928e8b2e
 			}
 	    } else {          // Desfaz modificações
 			for (neighbor = 0; neighbor < size_squared; ++neighbor) {   // Para cada vizinho do numero e.g. linha, coluna, caixa
@@ -152,7 +203,7 @@ int update(int value, int option) {  // Se option = 1 continua, se option = -1 b
 		}
 	}
 
-	return min_high << 16 | min_low;
+	return min_index;
 }
 
 void insert_clues(int buffer[]) {
@@ -175,9 +226,15 @@ int solve(int buffer[]) {
 	int i = 0, index, option = 1;
 	int solution = 0;
 
+<<<<<<< HEAD
 	int min = size_squared + 1, r2 = 0, c, cand = (size_squared+1)<<16;
+=======
+	int number, c, cand = (size_squared+1);
+>>>>>>> f6908ebd31eb5a8e993f0d9ecb083567928e8b2e
 
-	remaining_choices = calloc(board_size * 4, sizeof(int));
+	least_choices = size_squared + 1;
+
+	remaining_choices = calloc(board_size * 4, sizeof(int));	// Para cada bloco, linha, coluna - quantas possibilidades resta
 	choosen_constraint = calloc(board_size * size_squared, sizeof(int));
 
 	for (index = 0; index < board_size * 4; ++index) remaining_choices[index] = size_squared;
@@ -185,6 +242,7 @@ int solve(int buffer[]) {
 
 	insert_clues(buffer);
 
+<<<<<<< HEAD
 	// int current_row[board_size];
 	// int current_column[board_size];
 	//
@@ -192,11 +250,19 @@ int solve(int buffer[]) {
 	// 	current_row[index] = -1;
 	// 	current_column[index] = -1;
 	// }
+=======
+	int current_row[board_size];
+	current_column = calloc(board_size, sizeof(int));
+>>>>>>> f6908ebd31eb5a8e993f0d9ecb083567928e8b2e
 
 	int testoutudo[board_size*size_squared];
 	int current_row = 0;
 
+	struct Range vector_range[num_threads];
+    pthread_t thread[num_threads];
+
 	while(1) {
+<<<<<<< HEAD
 		while (1) {
 			for(i = current_row; i < board_size*size_squared; ++i) { // procura primeiro numero/posicao disponivel
 				if (testoutudo[i] == 0) break;
@@ -206,18 +272,75 @@ int solve(int buffer[]) {
 			} else {
 
 			}
+=======
+		while (i >= 0 && i < board_size - clues) {
+			if (option == 1) {
+				current_column[i] = cand;
+				least_choices = remaining_choices[cand];
+
+				if (least_choices > 1) {
+					printf("AQUI1\n");
+					int k;
+					for (k = 0; k < num_threads; ++k) {
+						printf("AQUI666\n");
+					    vector_range[k].initial_range = k * portion;
+					    vector_range[k].final_range = vector_range[k].initial_range + portion;
+
+					    if (k == (num_threads - 1)) vector_range[k].final_range = vector_size;
+
+					    pthread_create(&thread[i],	NULL,	Thread_solve,	&vector_range[k]);
+				    }
+				    for (k = 0; k != num_threads; ++k) {
+				    	pthread_join(thread[i],	NULL);
+				    }
+					printf("AQUI2\n");
+					// for (c = 0; c < board_size * 4; ++c) { // para cada uma das restrições
+					// 	if (remaining_choices[c] < least_choices) { // se a restrição for menor que least_choices, pego ela pois é a facil de resolver
+					// 		least_choices = remaining_choices[c];
+					// 		current_column[i] = c; // choose the top constraint - min = 9 (inicialmente) e cc[i] = c (0 inicialmente)
+					// 		if (least_choices <= 1) {
+					// 			break;
+					// 		}
+					// 	}
+					// }
+				}
+				if (least_choices == 0 || least_choices == (size_squared + 1)) {	// Se fora da faixa, backtrack
+					current_row[i--] = option = -1;
+				}
+			}
+			reference_row = current_column[i];
+			if (option == -1 && current_row[i] >= 0) {
+				update(constrainted_elements[c][current_row[i]], -1);
+			}
+			for (number = current_row[i] + 1; number < size_squared; ++number) {	// procura proximo numero disponivel
+				if (choosen_constraint[constrainted_elements[reference_row][number]] == 0) {
+					break;
+				}
+			}
+			if (number < size_squared) { // Pega o proximo numero disponivel e tenta
+				cand = update(constrainted_elements[reference_row][number], 1); // set the choice
+				current_row[i++] = number; option = 1; // Como deu certo, a linha escolhida = linha testada e dir = 1
+			} else {
+				current_row[i--] = option = -1; // Volta pra linha anterior e backtrack
+			}
+		}
+		if (i < 0) {
+			break;
+>>>>>>> f6908ebd31eb5a8e993f0d9ecb083567928e8b2e
 		}
 		// ++solution;
 		// --i;
 		// option = -1;
 	}
 
+	free(current_column);
 	free(choosen_constraint);
 	free(remaining_choices);
 
 	return solution;
 }
 
+<<<<<<< HEAD
 /*void *thread_solve() {
 	// escolhe o menor numero possivel
 	array de posicao cada uma guardando os numeros possiveis
@@ -259,4 +382,31 @@ int valid_position(int value) {
 	}
 
 	return valid;
+=======
+void *Thread_solve(void *arg) {
+	printf("AQUI5\n");
+	struct Range vector_r = *(struct Range*)arg;
+
+	int least_choices_temp = least_choices;
+
+	int i;
+	for (i = vector_r.initial_range; i < vector_r.final_range; ++i) { // para cada uma das restrições
+		if (remaining_choices[i] < least_choices) { // se a restrição for menor que least_choices, pego ela pois é a facil de resolver
+			least_choices_temp = remaining_choices[i];
+			current_column[i] = i;
+			if (least_choices <= 1) {
+				break;
+			}
+		}
+	}printf("AQUI4\n");
+	pthread_mutex_lock(&mutex);
+	printf("AQUI3\n");
+	if (least_choices_temp < least_choices) {
+		least_choices = least_choices_temp;
+		reference_row = i;
+	}
+	pthread_mutex_unlock(&mutex);
+
+	pthread_exit(NULL);
+>>>>>>> f6908ebd31eb5a8e993f0d9ecb083567928e8b2e
 }
