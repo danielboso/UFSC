4b8e943501270579c33ffc4f9f8f7d98e25b6b94
danielboso <bosodaniel@hotmail.com>
2017-10-28 21:02:39 -0200

    bucket sort

---
+++ Programação Concorrente/2017.2/bucket_sort/bucket_sort.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int int main(int argc, char** argv]) {
+
+	if(argc != 3) {
+		printf("Número de parâmetros incorreto!\n");
+		printf("Parâmetros:")
+		printf("	Tamanho do vetor.");
+		printf("	Número de buckets.");
+		printf("	Imprimir? '0' para não imprimir e '1' para imprimir.");
+	}
+}
