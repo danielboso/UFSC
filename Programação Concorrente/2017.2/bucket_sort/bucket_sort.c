97c8addb276a8d81ed0b71c1a8f4a2fa5fae7c2b
danielboso <bosodaniel@hotmail.com>
2017-11-08 21:25:56 -0200

    refatoração de código e início de implementação mpi

--- Programação Concorrente/2017.2/bucket_sort/bucket_sort.c
+++ Programação Concorrente/2017.2/bucket_sort/bucket_sort.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <stdio.h>
 #include "bucket_sort.h"
+#include <mpi.h>
 
 unsigned int tamvet;
 unsigned int nbuckets;
@@ -11,81 +12,119 @@ int flag_print;
 
 int main(int argc, char** argv) {
 
-	if(argc != 5) {
-		printf("Número de parâmetros incorreto!\n");
-		printf("Parâmetros:\n");
-		printf("	Tamanho do vetor.\n");
-		printf("	Número de buckets.\n");
-		printf("	Número de processos.\n");
-		printf("	Imprimir? '0' para não imprimir e '1' para imprimir.\n");
-		return 0;
-	}
+	int size;
+	int rank;
 
-	tamvet = atoi(argv[1]);
-	nbuckets = atoi(argv[2]);
-	nprocs = atoi(argv[3]);
-	flag_print = atoi(argv[4]);
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(&size);
+	MPI_Comm_rank(&rank);
 
-	int *vector = malloc(sizeof(int)*tamvet);
-	generateVector(vector, tamvet);
+// -----------------------------------------------------------------------------
+// 		Mestre
+// -----------------------------------------------------------------------------
+	if(rank == 0) {
 
-	Bucket * buckets = malloc(sizeof(Bucket) * nbuckets);
-	int bigger_number = vector[0];
-
-	int i;
-
-	for(i = 0; i < tamvet; i++) {
-		if(vector[i] > bigger_number) {
-			bigger_number = vector[i];
+		if(argc != 5) {
+			printf("Número de parâmetros incorreto!\n");
+			printf("Parâmetros:\n");
+			printf("	Tamanho do vetor.\n");
+			printf("	Número de buckets.\n");
+			printf("	Número de processos.\n");
+			printf("	Imprimir? '0' para não imprimir e '1' para imprimir.\n");
+			return 0;
 		}
-	}
 
-	for(i = 0; i < tamvet; i++) {
-		int index =getIndexBucket(vector[i], bigger_number);
-		if (index >= nbuckets) {
-			index = nbuckets-1;
+		tamvet = atoi(argv[1]);
+		nbuckets = atoi(argv[2]);
+		nprocs = atoi(argv[3]);
+		flag_print = atoi(argv[4]);
+
+		int *vector = malloc(sizeof(int)*tamvet);
+		generateVector(vector, tamvet);
+
+		Bucket * buckets = malloc(sizeof(Bucket) * nbuckets);
+		int bigger_number = vector[0];
+
+		int i;
+
+		for(i = 0; i < tamvet; i++) {
+			if(vector[i] > bigger_number) {
+				bigger_number = vector[i];
+			}
 		}
-		int size_bucket = buckets[index].size;
-		buckets[index].bucket = realloc(buckets[index].bucket, sizeof(int) * (size_bucket+1));
-		buckets[index].bucket[size_bucket] = vector[i];
-		buckets[index].size++;
-	}
 
-	int * index_bucket_in_tamvet = malloc(sizeof(int)*nbuckets);
-	int index = 0;
-	for(i = 0; i < nbuckets; i++) {
-		index_bucket_in_tamvet[i] = index;
-		index += buckets[i].size;
-		buckets[i].id = i;
-	}
+		for(i = 0; i < tamvet; i++) {
+			int index = getIndexBucket(vector[i], bigger_number);
+			if (index >= nbuckets) {
+				index = nbuckets-1;
+			}
+			int size_bucket = buckets[index].size;
+			buckets[index].bucket = realloc(buckets[index].bucket, sizeof(int) * (size_bucket+1));
+			buckets[index].bucket[size_bucket] = vector[i];
+			buckets[index].size++;
+		}
 
-	for(i = 0; i < nbuckets; i++) {
+		int * index_bucket_in_tamvet = malloc(sizeof(int)*nbuckets);
+		int index = 0;
+		for(i = 0; i < nbuckets; i++) {
+			index_bucket_in_tamvet[i] = index;
+			index += buckets[i].size;
+			buckets[i].id = i;
+		}
+
+		int bucket_to_send = 0;
+		int remaining_buckets = nbuckets;
+
+		for(i = 1; i < size && remaining_buckets != 0; i++) {
+			int * bucket = serializable_bucket(buckets[bucket_to_send]);
+
+			MPI_Send();
+			remaining_buckets--;
+		}
+
+		while(remaining_buckets != 0)  {
+
+			remaining_buckets--;
+		}
+
+		// Pega os buckets e os coloca no vetor
+		int j;
+		for(i = 0; i < nbuckets; i++) {
+			int index_bucket = index_bucket_in_tamvet[buckets[i].id];
+			for(j = 0; j < buckets[i].size; j++) {
+				if(buckets[i].size == 0) {
+					continue;
+				}
+				vector[index_bucket] = buckets[i].bucket[j];
+				index_bucket++;
+			}
+		}
+
+
+
+		// ---------------------------------------------------------------------
+		// 	A partir daqui, todas os bucket já devem estar ordenados e devolvidos
+		// para o mestre.
+		// ---------------------------------------------------------------------
+
+		if(flag_print == 1) {
+			printf("[");
+			for(i = 0; i < tamvet; i++) {
+				printf("%d ", vector[i]);
+			}
+			printf("]\n");
+		}
+
+		free(vector);
+		free(buckets);
+
+// -----------------------------------------------------------------------------
+// 		Escravo
+// -----------------------------------------------------------------------------
+	} else { // Escravos
+
 		qsort(buckets[i].bucket, buckets[i].size, sizeof(int), cmpfunc);
 	}
-
-	// Pega os buckets e os coloca no vetor
-	int j;
-	for(i = 0; i < nbuckets; i++) {
-		int index_bucket = index_bucket_in_tamvet[buckets[i].id];
-		for(j = 0; j < buckets[i].size; j++) {
-			if(buckets[i].size == 0) {
-				continue;
-			}
-			vector[index_bucket] = buckets[i].bucket[j];
-			index_bucket++;
-		}
-	}
-
-	if(flag_print == 1) {
-		printf("[");
-		for(i = 0; i < tamvet; i++) {
-			printf("%d ", vector[i]);
-		}
-		printf("]\n");
-	}
-
-	free(vector);
-	free(buckets);
 
 	return 0;
 }
