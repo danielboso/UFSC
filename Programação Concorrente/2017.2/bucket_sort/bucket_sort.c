b1f773c241c2d3e64e5ed387056780ff2cd8d7de
danielboso <bosodaniel@hotmail.com>
2017-11-11 20:18:05 -0200

    up

--- Programação Concorrente/2017.2/bucket_sort/bucket_sort.c
+++ Programação Concorrente/2017.2/bucket_sort/bucket_sort.c
@@ -12,11 +12,10 @@ int flag_print;
 
 int main(int argc, char** argv) {
 
-	int size;
 	int rank;
 
 	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 // -----------------------------------------------------------------------------
@@ -24,18 +23,18 @@ int main(int argc, char** argv) {
 // -----------------------------------------------------------------------------
 	if(rank == 0) {
 
-		if(argc != 5) {
+		if(argc != 4) {
 			printf("Número de parâmetros incorreto!\n");
 			printf("Parâmetros:\n");
 			printf("	Tamanho do vetor.\n");
 			printf("	Número de buckets.\n");
-			printf("	Número de processos.\n");
+			//printf("	Número de processos.\n");
 			printf("	Imprimir? '0' para não imprimir e '1' para imprimir.\n");
 			return 0;
 		}
 
 		tamvet = atoi(argv[1]);
-		nbuckets = atoi(argv[2]);
+		//nbuckets = atoi(argv[2]);
 		nprocs = atoi(argv[3]);
 		flag_print = atoi(argv[4]);
 
@@ -75,7 +74,7 @@ int main(int argc, char** argv) {
 		// Id do bucket que será enviado
 		int id_bucket_to_send = 0;
 		// Quantidade de buckets que ainda devem ser enviadas para os processos
-		int buckets_to_send = nbuckets;
+		int amount_buckets_to_send = nbuckets;
 		// Quantidade de buckets que se deve ficar esperando receber dos processos escravos
 		int buckets_to_receive = nbuckets;
 		// Flag que indica se posso continuar mandando buckets
@@ -89,8 +88,8 @@ int main(int argc, char** argv) {
 		//----------------------------------------------------------------------
 
 		// For que distribui primeira "leva" de buckets
-		for(i = 1; i < size && remaining_buckets != 0; i++) {
-			if(remaining_buckets == 0) {
+		for(i = 1; i < nprocs && amount_buckets_to_send != 0; i++) {
+			if(amount_buckets_to_send == 0) {
 				flag_continue_sending = 0;
 			}
 
@@ -100,8 +99,8 @@ int main(int argc, char** argv) {
 			// 		Control[0] - id
 			// 		Control[1] - size
 			// -----------------------------------------------------------------
-			control[0] = buckets[bucket_to_send].id;
-			control[1] = buckets[bucket_to_send].size;
+			control[0] = buckets[id_bucket_to_send].id;
+			control[1] = buckets[id_bucket_to_send].size;
 
 			// Envia a flag
 			MPI_Send(&flag_continue_sending, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
@@ -110,10 +109,10 @@ int main(int argc, char** argv) {
 			MPI_Send(&control, 2, MPI_INT, i, 0, MPI_COMM_WORLD);
 
 			// Envia vetor
-			MPI_Send(buckets[bucket_to_send].bucket, control[1], MPI_INT, i, 0, MPI_COMM_WORLD);
+			MPI_Send(buckets[id_bucket_to_send].bucket, control[1], MPI_INT, i, 0, MPI_COMM_WORLD);
 
-			remaining_buckets--;
-			bucket_to_send++;
+			amount_buckets_to_send--;
+			id_bucket_to_send++;
 		}
 
 		while(1) {
@@ -125,29 +124,29 @@ int main(int argc, char** argv) {
 
 			MPI_Recv(&control, 2, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
-			MPI_Recv(&buckets[control[0].bucket], MPI_INT, control[1], status.MPI_Source, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(buckets[control[0]].bucket, control[1], MPI_INT, status.MPI_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-			if(buckets_to_send > 0) {
+			if(amount_buckets_to_send > 0) {
 
-				control[0] = buckets[bucket_to_send].id;
-				control[1] = buckets[bucket_to_send].size;
+				control[0] = buckets[id_bucket_to_send].id;
+				control[1] = buckets[id_bucket_to_send].size;
 
 				// Envia a flag
-				MPI_Send(&flag_continue_sending, 1, MPI_INT, status.MPI_Source, 0, MPI_COMM_WORLD);
+				MPI_Send(&flag_continue_sending, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD);
 
 				// Envia dados de controle
-				MPI_Send(&control, 2, MPI_INT, status.MPI_Source, 0, MPI_COMM_WORLD);
+				MPI_Send(&control, 2, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD);
 
 				// Envia vetor
-				MPI_Send(buckets[bucket_to_send].bucket, control[1], MPI_INT, status.MPI_Source, 0, MPI_COMM_WORLD);
+				MPI_Send(buckets[id_bucket_to_send].bucket, control[1], MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD);
 
-				remaining_buckets--;
-				buckets_to_send++;
+				amount_buckets_to_send--;
+				id_bucket_to_send++;
 
 			} else {
 				// Manda mensagem para todos os processos pararem de aguardar mensagens
 				flag_continue_sending = 0;
-				MPI_Send(&flag_continue_sending, 1, MPI_INT, status.MPI_Source, 0, MPI_COMM_WORLD);
+				MPI_Send(&flag_continue_sending, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD);
 			}
 		}
 
@@ -192,7 +191,7 @@ int main(int argc, char** argv) {
 			// Atualiza valor da flag
 			MPI_Recv(&flag_continue_receiving, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-			if(continue_receiving == 1) {
+			if(flag_continue_receiving == 1) {
 
 				// Aloca espaço para o bucket
 				int control[2];
@@ -207,10 +206,10 @@ int main(int argc, char** argv) {
 				MPI_Recv(&control, 2, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
 				// Realoca espaço para os elementos do bucket
-				int * bucket = malloc(sizeof(int) * control[1])
+				int * bucket = malloc(sizeof(int) * control[1]);
 
 				// Recebe vetor
-				MPI_Recv(bucket.bucket, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+				MPI_Recv(bucket, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
 				// Ordena o vetor usando quick sort
 				qsort(bucket, control[1], sizeof(int), cmpfunc);
